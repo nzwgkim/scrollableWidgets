79aa24c19c555f813c091570d247f1ffa211726f
Woody <nz.wgkim@gmail.com>
2023-04-12 22:00:26 +1200

    Tabbar Styling

--- lib/screen/tabBar/basic_tab_bar_screen.dart
+++ lib/screen/tabBar/basic_tab_bar_screen.dart
@@ -8,18 +8,33 @@ class BasicAppbarTabbarScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-        length: TABS.length,
+        length: TABS.length * 3,
         child: Scaffold(
-            appBar: AppBar(
-          title: const Text('Basic Appbar Tabbar'),
-          bottom: TabBar(
-            tabs: TABS
-                .map((e) => Tab(
-                      icon: Icon(e.icon),
-                      child: Text(e.label),
-                    ))
-                .toList(),
+          appBar: AppBar(
+            title: const Text('Basic Appbar Tabbar'),
+            bottom: TabBar(
+              indicatorColor: Colors.yellow,
+              isScrollable: true,
+              tabs: [
+                ...TABS,
+                ...TABS,
+                ...TABS,
+              ]
+                  .map((e) => Tab(
+                        icon: Icon(e.icon),
+                        child: Text(e.label),
+                      ))
+                  .toList(),
+            ),
           ),
-        )));
+          body: TabBarView(
+            physics: const NeverScrollableScrollPhysics(),
+            children: [
+              ...TABS,
+              ...TABS,
+              ...TABS,
+            ].map((e) => Icon(e.icon)).toList(),
+          ),
+        ));
   }
 }
